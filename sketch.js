695e7599a2fda2468334f820a4583d2010b62fee
helloparthshah <helloparthshah@gmail.com>
2021-09-05 16:19:29 +0530

    Added files

---
+++ sketch.js
@@ -0,0 +1,11 @@
+// For it to run you need a local server (check: https://github.com/processing/p5.js/wiki/Local-server)
+
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // put drawing code here
+  background(220);
+}
